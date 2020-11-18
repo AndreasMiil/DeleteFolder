ef9cf3788980ef98e563a18e4a8fc45be50b05a4
Opilane TTHK <opilane@tthk.ee>
2020-11-18 15:00:18 +0200

    first commit

---
+++ NewProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace NewProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string rootPath = @"C:\Users\opilane\samples";
+            DeleteAllfiles();
+            DirectoryInfo rootDirectory = new DirectoryInfo(rootPath);
+            foreach(DirectoryInfo dir in rootDirectory.GetDirectories())
+            Console.WriteLine("Hello World!");
+        }
+        public static void DeleteAllfiles()
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            DirectoryInfo directory = new DirectoryInfo(rootPath);
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+        public static void DeleteAllfolders(string DirectoryName, bool ifExists)
+        {
+            if (Directory.Exists(DirectoryName))
+            {
+                Directory.Delete(DirectoryName, true);
+            }
+            else if (ifExists)
+            {
+                throw new SystemException("No such directory to delete");
+            }
+        }
+    }
+}
+
